2006595ccf8b0b54b33a7d7cd2466d464d031ae0
Fabian Lopez <flopez@hugeinc.com>
2015-11-30 18:22:00 -0500

    working webapp

--- src/main/java/com/hb/test/HomeController.java
+++ src/main/java/com/hb/test/HomeController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/home")
     public String welcome(Model model) {
+        System.out.println("FROM HOME CONTROLLER");
         model.addAttribute("greeting", "welcome");
         return "welcome";
     }
--- src/main/java/com/hb/test/config/AppWebConfig.java
+++ src/main/java/com/hb/test/config/AppWebConfig.java
@@ -1,25 +1,38 @@
 package com.hb.test.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+@Configuration
 public class AppWebConfig implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.setConfigLocation("/WEB-INF/spring/dispatcher-servlet.xml");
-//        AnnotationConfigWebApplicationContext annotationConfigWebApplicationContext = new AnnotationConfigWebApplicationContext();
-//        annotationConfigWebApplicationContext.register(AppConfig);
-        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", new DispatcherServlet(appContext));
+
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.scan("com.hb.test");
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(rootContext);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", dispatcherServlet);
         registration.setLoadOnStartup(1);
         registration.addMapping("/");
         System.out.println("REGISTERED SERVLET");
+    }
+
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
     }
 }
